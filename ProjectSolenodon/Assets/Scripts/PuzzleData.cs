6511ae939de48c5e5d3f160ca9def1bacf009355
John <jebillingham@gmail.com>
2019-11-08 15:45:02 -0800

    Change GameManager name to GameState. Change LevelManager to PuzzleGenerator. Replace references to level with Puzzle. Set PuzzleData members to private and add getters.

---
+++ ProjectSolenodon/Assets/Scripts/PuzzleData.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+/// <summary>
+/// The PuzzleData class contains data that is used when generating/loading
+/// a specific puzzle state onto the puzzle board scene.
+/// </summary>
+[Serializable]
+public class PuzzleData
+{
+    [SerializeField]
+    private int _numRegisterCards;
+    [SerializeField]
+    private int _numStackCards;
+    [SerializeField]
+    private int _numQueueCards;
+    [SerializeField]
+    private int _numHeapCards;
+
+    public int NumRegisterCards
+    {
+        get => _numRegisterCards;
+    }
+
+    public int NumStackCards
+    {
+        get => _numStackCards;
+    }
+
+    public int NumQueueCards
+    {
+        get => _numQueueCards;
+    }
+
+    public int NumHeapCards
+    {
+        get => _numHeapCards;
+    }
+
+    public PuzzleData(int numRegisterCards, int numStackCards, int numQueueCards, int numHeapCards)
+    {
+        _numRegisterCards = numRegisterCards;
+        _numStackCards = numStackCards;
+        _numQueueCards = numQueueCards;
+        _numHeapCards = numHeapCards;
+    }
+}
