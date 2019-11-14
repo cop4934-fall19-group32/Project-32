c09db85b8c436f4d68d71df220e1e2a849c8d077
Nic <nicolacognata@gmail.com>
2019-11-14 18:45:01 -0500

    Add basic interpeter and UI to PuzzleScene

---
+++ Assets/Scripts/Interpreter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interpreter : MonoBehaviour
+{
+
+	public GameObject SolutionPanel;
+
+	enum InstructionSet { 
+		
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private List<int> Instructions;
+}
