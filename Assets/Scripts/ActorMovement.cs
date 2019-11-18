f213af828d1a773e595a92d2ccd8c802b2ba7ebd
Brandy <brandy.king@knights.ucf.edu>
2019-11-17 20:21:17 -0500

    Update waypoints from array based movement to individual locations

--- Assets/Scripts/ActorMovement.cs
+++ Assets/Scripts/ActorMovement.cs
@@ -5,51 +5,92 @@ using UnityEngine;
 public class ActorMovement : MonoBehaviour
 {
     [SerializeField]
-    Transform[] waypoints;
+    Transform RestWaypoint;
+    [SerializeField]
+    Transform InputWaypoint;
+    [SerializeField]
+    Transform OutputWaypoint;
+    [SerializeField]
+    Transform RegisterWaypoint;
 
     [SerializeField]
     float moveSpeed = 2f;
-    // the starting position is at the "rest" waypoint
-    int waypointIndex = 2;
+
+    int x;
+
 
     void Start()
     {
-        transform.position = waypoints [waypointIndex].transform.position;
+        x = 0;
+        transform.position = RestWaypoint.transform.position;
     }
 
     void Update()
     {
         // testing movement
-        Move();
+        // this is a simple loop to illustrate the movement of the actor
+        if (x == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            InputWaypoint.transform.position,
+            moveSpeed * Time.deltaTime);
+            if (transform.position == InputWaypoint.transform.position)
+                x++;
+        }
+        if (x == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            OutputWaypoint.transform.position,
+            moveSpeed * Time.deltaTime);
+            if (transform.position == OutputWaypoint.transform.position)
+                x++;
+        }
+        if (x == 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            RestWaypoint.transform.position,
+            moveSpeed * Time.deltaTime);
+            if (transform.position == RestWaypoint.transform.position)
+                x++;
+        }
+        if (x == 3)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            RegisterWaypoint.transform.position,
+            moveSpeed * Time.deltaTime);
+            if (transform.position == RegisterWaypoint.transform.position)
+                x++;
+        }
+        if (x == 4)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            RestWaypoint.transform.position,
+            moveSpeed * Time.deltaTime);
+            if (transform.position == RestWaypoint.transform.position)
+                x++;
+        }
+        if (x == 5)
+            x = 0;
 
         /* the following code will be used to interpret commands and move to the designated waypoints accordingly
          * 
          * if (instruction is INPUT)
-         *      waypointIndex = 0;
+         *      transform.position = Vector2.MoveTowards(transform.position,
+         *                          InputWaypoint.transform.position,
+         *                          moveSpeed * Time.deltaTime);
          * if (instruction is OUTPUT)
-         *      waypointIndex = 1;
-         * if (instruction is {card related})
-         *      waypointIndex = 3;
+         *      transform.position = Vector2.MoveTowards(transform.position,
+         *                          OutputWaypoint.transform.position,
+         *                          moveSpeed * Time.deltaTime);
+         * if (instruction is {register related})
+         *      transform.position = Vector2.MoveTowards(transform.position,
+         *                          RegisterWaypoint.transform.position,
+         *                          moveSpeed * Time.deltaTime);
          * else
-         *      waypointIndex = 2;
-         *      
-         * transform.position = Vector2.MoveTowards(transform.position,
-         *                          waypoints[waypointIndex].transform.position,
-         *                          moveSpeed * Time.deltaTime);     
+         *      transform.position = Vector2.MoveTowards(transform.position,
+         *                          RestWaypoint.transform.position,
+         *                          moveSpeed * Time.deltaTime);
          *      
          */
-    }
-
-    void Move()
-    {
-        transform.position = Vector2.MoveTowards(transform.position,
-                                waypoints[waypointIndex].transform.position,
-                                moveSpeed * Time.deltaTime);
-
-        if (transform.position == waypoints [waypointIndex].transform.position)
-            waypointIndex++;
-
-        if (waypointIndex == waypoints.Length)
-            waypointIndex = 0;
     }
 }
