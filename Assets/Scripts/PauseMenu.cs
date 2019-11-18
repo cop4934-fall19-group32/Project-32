3097a4584ad0f7197a3bd67f243b9abc87c4e9b0
Sean Simonian <SeanSimonian@disroot.org>
2019-11-18 05:41:09 -0500

    Add UI to puzzle scene, create pause menu and functionality

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+
+	// public static bool paused = false;
+	public GameObject pauseMenu;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			TogglePause();
+		}
+	}
+
+	public void TogglePause()
+	{
+		// Check if pause menu is currently active in scene, toggle the active state, and pause/resume time
+		bool pause = !pauseMenu.activeInHierarchy;
+		pauseMenu.SetActive(pause);
+		Time.timeScale = pause ? 0f : 1f;
+	}
+
+
+	public void ExitScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("QUIT GAME");
+		Application.Quit();
+	}
+
+}
